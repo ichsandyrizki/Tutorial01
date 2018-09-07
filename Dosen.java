d101f8b5acf8041b3a31ba6eb1a4a3438837889b
ichsandy <ichsandyrizky22@gmail.com>
2018-09-07 14:38:18 +0700

    add constructor

---
+++ Dosen.java
@@ -0,0 +1,6 @@
+public class Dosen{
+	 public static void main (String[] args)
+	    {
+		 	Dosen dosen = new Dosen();
+	    }
+}
---
+++ Mahasiswa.java
@@ -0,0 +1,6 @@
+public class Mahasiswa{
+	 public static void main (String[] args)
+	    {
+		 	Mahasiswa mahasiswa = new Mahasiswa();
+	    }
+}
